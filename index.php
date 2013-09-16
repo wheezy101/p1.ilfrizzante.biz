45755fd0d9ee19f85f6bad441b2bbbe66caf0807
Nina Tobio <nina.tobio@gmail.com>
2013-09-16 14:00:15 -0400

    Starting About Me page

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-Welcome to P1!
+<!DOCTYPE HTML>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/aboutme.css" type="text/css" />
+<title>About Me</title>
+<h2>About Me</h2>
+<p>My name is Nina. I work as an economic researcher. I enjoy watching movies, reading, follwing the Pats, and traveling.</p>
+<p>I am using Komodo Edit as my code editor.</p>
+</head>
+<body>
+    
+</body>
