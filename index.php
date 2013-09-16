2b04950aaf5b4b4eb996ca9cf29f610ad8ba8530
Nina Tobio <nina.tobio@gmail.com>
2013-09-16 15:20:40 -0400

    Adding content

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <!DOCTYPE HTML>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" href="css/aboutme.css" type="text/css" />
-<title>About Me</title>
-<h2>About Me</h2>
-<p>My name is Nina. I work as an economic researcher. I enjoy watching movies, reading, follwing the Pats, and traveling.</p>
-<p>I am using Komodo Edit as my code editor.</p>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />   
+    <link rel="stylesheet" href="css/aboutme.css" type="text/css" />
+    <title>About Me</title>
 </head>
 <body>
-    
+    <h2>About Me</h2>
+    <p>My name is Nina. I work as an economic researcher. I enjoy watching movies, reading, following the Pats, and traveling.</p>
+    <p>I am using Komodo Edit as my code editor.</p> 
 </body>
